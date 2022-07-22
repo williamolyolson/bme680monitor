dcc606580fd5bdf6b4ddb43c2e5689b7e6b1a2ee
williamolyolson <85414190+williamolyolson@users.noreply.github.com>
2022-07-22 10:54:51 -0700

    Update bme680.py

--- bme680.py
+++ bme680.py
@@ -6,7 +6,7 @@ import adafruit_bme680
 #degc to degf
 def c_to_f(c):
         return c * 9.0 / 5.0 + 32.0
-
+tempoffset=-4
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 #print('Temperature: {} degrees C'.format(sensor.temperature))
@@ -17,7 +17,7 @@ outputfile = '/var/www/html/index.html'
 outfile = open(outputfile,'w')
 outfile.write('<html><head></head><body>')
 outfile.write('<table><tr><th>Humidity(%)</th><td>{}</td>' .format(round(sensor.humidity,2)))
-outfile.write('<th>Temperature(F)</th><td>{}</td></tr>'.format(round(c_to_f(sensor.temperature),2)))
+outfile.write('<th>Temperature(F)</th><td>{}</td></tr>'.format(round(c_to_f(sensor.temperature+tempoffset),2)))
 outfile.write('<tr><th>Gas(ohms)</th><td>{}</td>'.format(sensor.gas))
 outfile.write('<th>Pressure(hpa)</th><td>{}</td></tr>'.format(round(sensor.pressure,2)))
 outfile.write('<tr><th>Time Stamp</th><td>{}</td></tr>'.format(datetime.now(tz=None)))
